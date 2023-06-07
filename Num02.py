7ee707c21c6bedf7a77c05e0b1d541d7cbe5827f
Svetlana <medjog@gmail.com>
2023-06-08 00:31:43 +0400

    Решение второй задачи

--- Num02.py
+++ Num02.py
@@ -5,3 +5,16 @@
 # *Пример:*
 #2 2
 # 4 
+def summa(number1, number2):
+    if number1 == 0:
+        return number2
+    if number2 == 0:
+        return number1
+    return (summa(number1 - 1, number2 + 1))
+
+num1 = int(input('Введите число: '))
+num2 = int(input('Введите число: '))
+print(f'Сумма числел {num1} и {num2} равна {summa(num1, num2)}')
+
+
+
