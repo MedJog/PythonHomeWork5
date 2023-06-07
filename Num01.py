26ab263c3c23866500d07c44b214d13efdecc616
Svetlana <medjog@gmail.com>
2023-06-08 00:09:36 +0400

    Решение первой задачи

--- Num01.py
+++ Num01.py
@@ -3,3 +3,14 @@
 # *Пример:*
 # A = 3; B = 5 -> 243 (3⁵)
 # A = 2; B = 3 -> 8 
+
+def power(number, extent):
+    if number == 0 or extent == 0 or number == 1:
+        return 1
+    if extent == 1:
+        return number
+    return (number * (power(number, extent - 1)))
+
+num = int(input('Введите число: '))
+ext = int(input('Введите значеие степени: '))
+print(f' {num} в степени {ext} равно {power(num, ext)}.')
